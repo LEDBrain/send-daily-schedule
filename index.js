e12310763bab8a39af2f213b010614b6c4d7a1e0
Ron31 <ron3108@gmx.net>
2020-09-13 18:06:06 +0200

    🚑🐛 Fixed a major
    Fixed:
    - Incorrect behavior with schedules

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ toPascalCase = string => {
 const getWeek = (date = new Date()) => {
     date.setHours(0, 0, 0, 0);
     // Thursday in current week decides the year.
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    const wdate = new Date();
+    wdate.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
     // January 4 is always in week 1.
-    const week1 = new Date(date.getFullYear(), 0, 4);
+    const week1 = new Date(wdate.getFullYear(), 0, 4);
     // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+    return 1 + Math.round(((wdate.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
 };
 
 const send = async (to, subject, content) => {
